34425ba73c8be5615ce7d433bbad7eb70c82cb41
yujung0 <137154455+yujung0@users.noreply.github.com>
2025-12-24 14:07:05 +0900

    JAVA_Part02_05,06

---
+++ src/Part2/HashMapExample.java
@@ -0,0 +1,32 @@
+package Part2;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        // 추가
+        hashMap.put("A", "a");
+        hashMap.put("B", "b");
+        System.out.println(hashMap);
+
+        // 삭제
+        hashMap.remove("A");
+        System.out.println(hashMap);
+
+        // 중복 값 추가
+        hashMap.put("B", "bbbb");
+        System.out.println(hashMap);
+
+        // 조회
+        System.out.println(hashMap.get("B"));
+        System.out.println(hashMap.get("C"));
+        System.out.println(hashMap.getOrDefault("C", "default"));
+
+        
+
+
+    }
+}
---
+++ src/Part2/NumberInputHandler.java
@@ -0,0 +1,33 @@
+package Part2;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NumberInputHandler  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("입력할 숫자. (종료 : exit)");
+
+        while (true) {
+            System.out.print("입력 : ");
+            String input = scanner.nextLine().trim();
+
+
+            if (input.equals("exit")) {
+                System.out.println("프로그램 종료");
+                break;
+            }
+
+            try {
+                int number = Integer.parseInt(input);
+                System.out.println("변환 성공. 입력한 숫자는 " + number + " 입니다.");
+            } catch (NumberFormatException e) {
+                System.out.println("숫자가 아닙니다. 올바른 숫자를 입력하세요.");
+            } finally {
+                System.out.println("finally 사용");
+            }
+
+        }
+            scanner.close();
+    }
+}
